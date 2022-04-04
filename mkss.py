1a7084c26f076aab2d4dce8ad73826a0620745a9
mariomadproductions <9919020+mariomadproductions@users.noreply.github.com>
2022-04-04 20:33:54 +0100

    Refactoring
    Moved main code into mkss function
    Change from sys.argv to argparse
    Changed command line interface to use more common design
    Change chmod from subprocess call to os call
    Added argparse to python template

--- mkss.py
+++ mkss.py
@@ -1,25 +1,36 @@
 #!/usr/bin/env python3
-from sys import argv, platform
-import subprocess
+import argparse
+import os
+
+TEMPLATE_DICT = {"sh": "#!/usr/bin/env bash\n",
+                 "py": "#!/usr/bin/env python3\n"\
+                       "import argparse\n\n"\
+                       "def main():\n    "\
+                       "parser = argparse.ArgumentParser()\n    "\
+                       "args = parser.parse_args()"\
+                       "\n\nif __name__ == '__main__':\n    main()\n"}
+
+FILE_TYPES = list(TEMPLATE_DICT.keys())
+
+#from https://stackoverflow.com/a/30463972
+def make_executable(path):
+    mode = os.stat(path).st_mode
+    mode |= (mode & 0o444) >> 2    # copy R bits to X
+    os.chmod(path, mode)
+
+def mkss(file_path,file_type):
+
+    with open(file_path, 'x') as file:
+        file.write(TEMPLATE_DICT[file_type])
+
+    make_executable(file_path)
 
 def main():
-    if len(argv) == 3:
-        destfile_path=argv[-2]
-        filetype=argv[-1]
-    elif len(argv) == 2:
-        destfile_path=argv[-1]
-        filetype='sh'
-    else:
-        print('Invalid number of arguments')
-
-    template_dict = {"sh": "#!/usr/bin/env bash",
-                     "py": "#!/usr/bin/env python3\n\ndef main():\n    "\
-                            "pass\n\nif __name__ == '__main__':\n    main()"}
-
-    with open(destfile_path,'x') as destfile:
-        destfile.write("{}\n".format(template_dict[filetype]))
-        if platform.startswith('linux'):
-            subprocess.run(['chmod','+x',destfile_path])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--type', default='sh', choices=FILE_TYPES)
+    parser.add_argument('file')
+    args = parser.parse_args()
+    mkss(args.file,args.type)
 
 if __name__ == "__main__":
     main()
