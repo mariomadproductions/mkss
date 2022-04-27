062eca765d5a6d45fa7aee11990880102aca26d8
mariomadproductions <9919020+mariomadproductions@users.noreply.github.com>
2022-04-27 22:09:42 +0100

    Move templates to individual text files

--- mkss.py
+++ mkss.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import os
+from pathlib import Path
 
 #from https://stackoverflow.com/a/30463972
 def make_executable(path):
@@ -13,15 +14,17 @@ def mkss(file_path,file_type,template_dict):
         file.write(template_dict[file_type])
     make_executable(file_path)
 
+def get_template_dict(template_dir_path):
+    template_dict = {}
+    for file_path in template_dir_path.iterdir():
+        with open(file_path, 'rt') as file:
+            template_dict[file_path.name] = file.read()
+
+    return template_dict
+    
 def main():
-    template_dict = {"sh": "#!/usr/bin/env bash\n",
-                     "py": "#!/usr/bin/env python3\n"\
-                     "import argparse\n\n"\
-                     "def main():\n"\
-                     "    parser = argparse.ArgumentParser()\n"\
-                     "    parser.add_argument('example')\n"\
-                     "    args = parser.parse_args()\n"\
-                     "\nif __name__ == '__main__':\n    main()\n"}
+    template_dict = get_template_dict(Path(__file__).resolve().parent\
+                                      .joinpath('templates'))
 
     file_types = list(template_dict.keys())
     
